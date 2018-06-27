e4497b1345fc9b7adbe6da0b94c66cc5712af142
Maris <maris@tammik.ca>
2018-06-27 16:54:18 -0700

    - initial setup with instance mesh renderers moving in random directions

---
+++ Unity/Assets/Scripts/Bootstrap.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Rendering;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+public class Bootstrap : MonoBehaviour {
+    public static Bootstrap Instance { get; private set; }
+    public GameObject prefab;
+    public int count = 1024;
+    [Range(-10f, 10f)]
+    public float speed = 1f;
+
+    void Awake () {
+        Instance = this;
+        var entityManager = World.Active.GetExistingManager<EntityManager>();
+        float range = 10f;
+
+        // setup GameObject entities
+        EntityArchetype archetype = entityManager.CreateArchetype(
+            ComponentType.Create<Position>(), 
+            ComponentType.Create<Direction>(),
+            ComponentType.Create<TransformMatrix>());
+        //for (int i = 0; i < count; i++)
+        //{
+        //    var go = Instantiate(prefab, transform);
+        //    var entity = GameObjectEntity.AddToEntityManager(entityManager, go);
+        //    var position = new PositionComponent()
+        //    {
+        //        value = 
+        //    };
+        //    var direction = new Direction()
+        //    {
+        //        value = Random.insideUnitSphere
+        //    };
+        //    entityManager.AddComponentData(entity, position);
+        //    entityManager.AddComponentData(entity, direction);
+        //}
+        
+        MeshInstanceRenderer meshInstanceRenderer = new MeshInstanceRenderer();
+        var proto = GameObject.Instantiate(prefab);
+        meshInstanceRenderer = proto.GetComponent<MeshInstanceRendererComponent>().Value;
+        GameObject.Destroy(proto);
+
+        for (int j = 0; j < count; j++)
+        {
+            var e = entityManager.CreateEntity(archetype);
+            //var posData = new Position();
+            //posData.Value.xyz = new Vector3(
+            //    Random.Range(-range, range), 
+            //    Random.Range(-range, range), 
+            //    Random.Range(-range, range));
+            //entityManager.SetComponentData<Position>(e, posData);
+            entityManager.SetComponentData<Direction>(e, new Direction()
+                { Value = new float3(Random.insideUnitSphere) });
+            entityManager.AddSharedComponentData(e, meshInstanceRenderer);
+        }
+    }
+}
---
+++ Unity/Assets/Systems/JobMovementSystem.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Collections;
+using Unity.Transforms;
+using Unity.Burst;
+
+namespace SimpleExample.ECS
+{	
+	public class JobMovementSystem : JobComponentSystem
+	{
+		[BurstCompile]
+		struct MovementJob : IJobProcessComponentData<Position, Direction>
+		{
+            [ReadOnly] public float deltaTime;
+            [ReadOnly] public float speed;
+            public void Execute(ref Position position, [ReadOnly]ref Direction dir)
+			{
+				position.Value = position.Value + (dir.Value * deltaTime * speed);
+			}
+		}
+
+		protected override JobHandle OnUpdate(JobHandle inputDeps)
+		{
+			var job = new MovementJob()
+            {
+                deltaTime = Time.deltaTime,
+                speed = Bootstrap.Instance.speed
+            };
+			return job.Schedule(this, 64, inputDeps); 
+		} 
+	}
+}
